d311d32b80158a986425863ad423f36c36e00b8e
José Neto <ribeirojoseph44@gmail.com>
2022-12-14 12:09:59 -0300

    Configurações com Prisma

---
+++ src/prisma.service.ts
@@ -0,0 +1,17 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    // Forçando a conexão com o banco de dados ao iniciar a aplicação
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    // Derrubando a aplicação quando o banco cair
+    async enableShutdownHooks(app: INestApplication) {
+        this.$on('beforeExit', async () => {
+            await app.close();
+        });
+    }
+}
